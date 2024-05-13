cfd9ccfc8d617b06ebfd7a3d2a7968ec71c7c450
ivanbel1 <154052152+ivanbel1@users.noreply.github.com>
2024-05-13 23:23:16 +0300

    Создали первоначальный массив

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+using Microsoft.VisualBasic;
+// See https://aka.ms/new-console-template for more information
+
+
+
+
+string[] array2 = ["Вася", "Олег", "8", "12", "Дмитрий"];
+Console.WriteLine("Ваш первоначальный массив : " + string.Join(",", array2));
+
+
+
+for (int i = 0; i < array2.Length; i++)
+{
+    if (array2[i].Length > 3)
+    {
+        Array.Resize(ref array2, 3);
+    }
+}
+
+Console.WriteLine("Результат : " + string.Join(",", array2));
